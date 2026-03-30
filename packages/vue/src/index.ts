67c81eb6dcca77bd0b90e612b098d2ba07cb98c3
재욱 <haensol976520@gmail.com>
2026-03-30 09:14:35 +0900

    feat(vue): export public API

---
+++ packages/vue/src/index.ts
@@ -0,0 +1,4 @@
+export { default as VirtualList } from "./components/VirtualList.vue";
+export { default as InfiniteList } from "./components/InfiniteList.vue";
+export { useInfinitePages } from "./composables/useInfinitePages";
+export type { VirtualListProps, InfiniteListProps, ItemStyle } from "./types";
